382d07b24182e6be17a361b03ec50f5afe9d7205
Mirami5114 <125982089+Rahul5114@users.noreply.github.com>
2024-07-01 22:23:52 +0530

    Create java-string.java

---
+++ Hackerrank/java-string.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        String result = "";
+        
+        char[] charArray = S.toCharArray();
+        
+        for(int i= start; i<end;i++){
+            
+            result += charArray[i];
+            
+        }
+        System.out.println(result);
+            
+        
+    }
+}
